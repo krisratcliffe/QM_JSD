16199a21147a49e84f0cdf6929deb592b8807118
Kris Ratcliffe <krisratcliffe@bpp.com>
2022-06-08 17:14:08 +0100

    Dynamically add splitscreen.js

--- testScript.js
+++ testScript.js
@@ -1,1 +1,11 @@
-document.write('<script src="https://cdn.jsdelivr.net/gh/krisratcliffe/QM_JSD/includedscripts/SplitScreen.js" type="text/javascript"></script>');
+var splitScreenAdded = 0;
+$('div[data-hookscript="SplitScreen"]').each(function() {
+    if (splitScreenAdded = 0) {
+        var s = document.createElement( 'script' );
+        s.setAttribute( 'src', "https://cdn.jsdelivr.net/gh/krisratcliffe/QM_JSD/includedscripts/SplitScreen.js" );
+        document.body.appendChild( s );   
+        this.append("<script>SplitScreen()</script>")
+        var splitScreenAdded = 1;
+        SplitScreen();
+    }    
+});
