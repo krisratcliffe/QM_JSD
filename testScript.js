6cf58716502ec49f0849656d2e21905734c2c5c3
Kris Ratcliffe <krisratcliffe@bpp.com>
2022-06-09 10:07:41 +0100

    Remove limit check

--- testScript.js
+++ testScript.js
@@ -1,12 +1,9 @@
-var splitScreenAdded = 0;
-$('div[data-hookscript="SplitScreen"]').each(function() {
-    if (splitScreenAdded = 0) {
+
+$('div[data-hookscript="SplitScreen"]').each(function() {   
         var s = document.createElement( 'script' );
         s.setAttribute( 'src', "https://cdn.jsdelivr.net/gh/krisratcliffe/QM_JSD/includedscripts/SplitScreen.js" );
         document.body.appendChild( s );   
         this.append("<script>SplitScreen()</script>")
         var splitScreenAdded = 1;
-        SplitScreen();
-        //xys
-    }    
+        SplitScreen(); 
 });
